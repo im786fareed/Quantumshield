78d510ec08a0f54bebe67edd75cf5dc74a5bc6c9
Fareed Shaik <im786fareed@gmail.com>
2026-01-04 22:55:38 +0530

    FORCED: Fixed root page to show Defense Terminal and Traces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import { redirect } from 'next/navigation';
+'use client';
+import HomePage from './home/page';
 
 export default function RootPage() {
-  redirect('/home');
+  // This forces the "Defense Terminal" to be the first thing people see
+  return <HomePage />;
 }
